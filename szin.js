597f29f5b6dd7899d9a4a220162783ee637291be
szt2GémesSzabolcs <115459925+Szabi20@users.noreply.github.com>
2023-09-22 09:46:08 +0200

    Add files via upload

---
+++ szin.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    let item   = document.querySelector(".item");
+    let width =document.getElementById("#szinkocka").value;
+    let height =document.getElementById("#szam").value;
+    let color   = document.getElementById("szinkocka").value;
+    console.log("color:"+color);
+    let szam=document.getElementById("szam").value;
+    console.log("szam"+szam)
+    let budy = document.querySelector(".item")
+    item.style.width=width
+    item.style.height=height
+}
+
+
+
+
+
+
+
+
+
